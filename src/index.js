0690002635a64432fe9bd7eef4c4e64abbb6b5b9
nogadas-vonryan <vonnogadas251@gmail.com>
2025-02-18 00:01:43 +0800

    add interactivity

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, ipcMain } from 'electron';
 import path from 'node:path';
 import started from 'electron-squirrel-startup';
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import { randomInt } from 'node:crypto';
+import fs  from 'node:fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+let mainWindow;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
@@ -16,7 +18,7 @@ if (started) {
 
 const createWindow = () => {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 600,
     height: 450,
     webPreferences: {
@@ -25,11 +27,26 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'pages/oiia.html'));
+  mainWindow.loadFile(path.join(__dirname, 'pages/splashscreen.html'));
   mainWindow.maximize();
   mainWindow.menuBarVisible = false;
 
   mainWindow.fullScreen = true;
+    
+  // Listen for IPC event to open DevTools
+  ipcMain.on('open-devtools', () => {
+    if (mainWindow) {
+      console.log('Page Loaded! Now printing...'); // Debugging line
+      mainWindow.webContents.printToPDF({ marginsType: 0, printBackground: true,
+        pageSize: { width: 3.3, height: 7.09 }
+       })
+      .then(data => {
+          fs.writeFileSync('output.pdf', data);
+          console.log('PDF Saved as output.pdf');
+      })
+      .catch(error => console.error(`PDF Generation Failed: ${error}`));
+    }
+  });
 };
 
 // This method will be called when Electron has finished
@@ -37,7 +54,21 @@ const createWindow = () => {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow();
+  mainWindow.webContents.openDevTools();
 
+  mainWindow.webContents.on('did-navigate', (event, url) => {
+    if (!url.includes('/form.html')) return;
+
+    console.log('Page Loaded! Now printing...'); // Debugging line
+    mainWindow.webContents.printToPDF({ marginsType: 0, printBackground: true,
+      pageSize: { width: 227, height: 510 }
+     })
+    .then(data => {
+        fs.writeFileSync('output.pdf', data);
+        console.log('PDF Saved as output.pdf');
+    })
+    .catch(error => console.error(`PDF Generation Failed: ${error}`));
+  });
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   app.on('activate', () => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
     },
   },
   daisyui: {
-    themes: ["light"],
+    themes: ["light","dark"],
   },
   plugins: [
     require("daisyui")
